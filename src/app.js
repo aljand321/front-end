df24ebb6f2ff04aa66309dcd6c11e6ac39917988
Alvaro Willams Alejandro Arancibia <aljand321@gmail.com>
2019-05-02 15:51:36 -0400

    primer commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+//importing routes
+const indexRoutes = require('./routes/index');
+
+
+//setings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+//MIDELWARES
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.use('/', indexRoutes);
+
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+  });
