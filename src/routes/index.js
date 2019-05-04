3b6c11f1c8749b33d8413e94edc35e8705dc8fd5
Alvaro Willams Alejandro Arancibia <aljand321@gmail.com>
2019-05-04 18:41:23 -0400

    añadiendo crud a mi diseño

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,21 +3,22 @@ const fetch = require('node-fetch');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+router.get('/home', (req, res) => {
     fetch('http://localhost:3000/api/servicios')
     .then(resp => resp.json())
     .then(resp =>{
-      res.render('index',{
+      res.render('home',{
         resp
       });
   });
 });
 
-router.get('/servicios', (req, res) => {
-  res.render('Servicios');
-});
-
 //esta ruta sirve para enviar datos a la tabla servicios
-router.post('/servico', (req, res) => {
+router.post('/servicio', (req, res) => {
   var enviar = {
     descripcion: req.body.descripcion,
     sigla: req.body.sigla,
@@ -34,8 +35,53 @@ router.post('/servico', (req, res) => {
     .catch(error => console.error('Error:', error))
     .then(data => {
       console.log(data);
-      res.send(data);
+      res.redirect('/servicio');
     })
+});
+router.get('/servicio', (req, res) => {
+  fetch('http://localhost:3000/api/servicios')
+    .then(resp => resp.json())
+    .then(resp =>{
+      res.render('servicios',{
+        resp
+      });
+  });
+});
+
+//servcio para mostrar solo un Servicio
+router.get('/api/servOne/:id', (req, res) => {
+  var id = req.url;
+  
+  fetch('http://localhost:3000' + id )
+    .then(resp => resp.json())
+    .then(resp =>{
+      console.log(resp + "    <<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
+      res.render('updateServcios',{
+        resp
+      });
+  });
+});
+router.post('/api/servicios/:id', (req,res) =>{
+  var id = req.url;
+  console.log(id + " esto es el id");
+  var update = {
+    descripcion: req.body.descripcion,
+    sigla: req.body.sigla
+  }
+  var esto={
+    method: 'POST',
+    body: JSON.stringify(update),
+    headers:{
+      'Content-type' : "application/json"
+    }
+  };
+  fetch('http://localhost:3000'+id,esto)
+  .then(res => res.json())
+  .catch(error => console.error('Error:', error))
+  .then(data => {
+    console.log(data);
+    res.redirect('/servicio');
+  })
 });
 
 //<%- include("partials/head")%>
